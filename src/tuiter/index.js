f880a224c96b254eb8b2e8e1b7037e4c3cc48152
arushagarwal <agarwal.arush98@gmail.com>
2023-03-01 12:45:28 -0500

    assignment 6 tuiter done

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,12 +1,22 @@
 import {Link} from "react-router-dom";
+import NavigationSidebar from "./navigation-sidebar";
+import WhoToFollowList from "./who-to-follow-list";
+import ExploreComponent from "./explore";
+import PostSummaryList from "./post-summary-list";
 
 function Tuiter() {
   return(
-      <div>
-        <Link to="/">Lab</Link> |
-        <Link to="/hello">Hello</Link> |
-        <Link to="/tuiter">Tuiter</Link>
-        <h1>Tuiter</h1>
+      <div className="row mt-2">
+        <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+          <NavigationSidebar active="explore"/>
+        </div>
+        <div className="col-10 col-md-10 col-lg-7 col-xl-6"
+             style={{"position": "relative"}}>
+          <ExploreComponent/>
+        </div>
+        <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+          <WhoToFollowList/>
+        </div>
       </div>
   )
 }
