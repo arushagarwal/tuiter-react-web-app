d399e92d4f48a336a4a1a3cb6a47e9b584e5c518
arushagarwal <agarwal.arush98@gmail.com>
2023-03-01 12:13:58 -0500

    assignment 6 lab done

---
+++ src/labs/index.js
@@ -0,0 +1,13 @@
+import Assignment6 from "./a6";
+import ConditionalOutput from "./a6/conditional-output";
+import TodoList from "./a6/todo/todo-list";
+function Labs() {
+  return(
+      <div>
+        <Assignment6/>
+        <ConditionalOutput/>
+        <TodoList/>
+      </div>
+  );
+}
+export default Labs;
