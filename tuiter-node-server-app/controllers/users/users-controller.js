6d66f43b86e2454b847e11bbaa8a9ca2985ef222
Arush Agarwal <arushagarwal@Arushs-MacBook-Air-2.local>
2023-03-26 22:54:35 -0400

    a8 part 5 done

--- tuiter-node-server-app/controllers/users/users-controller.js
+++ tuiter-node-server-app/controllers/users/users-controller.js
@@ -4,6 +4,36 @@ let users=people;
 const UserController = app => {
     app.get('/api/users',findUsers);
     app.get('/api/users/:uid', findUserById);
+    app.post('/api/users',createUser);
+    app.delete('/api/users/:uid',deleteUser);
+    app.put('/api/users/:uid', updateUser);
+}
+
+const updateUser = (req, res) => {
+    const userId = req.params['uid'];
+    const updates = req.body;
+    users = users.map((usr) =>
+        usr._id === userId ?
+            {...usr, ...updates} :
+            usr
+    );
+    res.sendStatus(200);
+}
+
+
+const deleteUser = (req, res) => {
+    const userId = req.params['uid'];
+    users = users.filter(usr => usr._id !== userId);
+    res.sendStatus(200);
+}
+
+
+const createUser = (req,res) => {
+    const newUser = req.body;
+    console.log(req.body);
+    newUser._id = (new Date()).getTime() + '';
+    users.push(newUser);
+    res.json(newUser);
 }
 
 const findUsers = (req,res) => {
@@ -18,7 +48,6 @@ const findUsers = (req,res) => {
 
 const findUserById = (req, res) => {
     const userId = req.params.uid;
-    console.log(req.params);
     const user = users.find(u => u._id === userId);
     res.json(user);
 }
